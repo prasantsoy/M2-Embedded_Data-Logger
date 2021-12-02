b9395c2b8960ddaad84339a1a0978f2822548287
prasantsoy <94223490+prasantsoy@users.noreply.github.com>
2021-12-02 10:11:46 +0530

    Delete func.h

--- 3_Implimentation/inc/func.h
+++
@@ -1,8 +0,0 @@
-#include <avr/io.h>
-
-#define enable            1
-#define registerselection 0
-
-void send_a_command(unsigned char command);
-void send_a_character(unsigned char character);
-void send_a_string(char *string_of_characters);
