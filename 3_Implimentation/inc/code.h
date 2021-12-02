5527008a50d603b46e9552342fc153d9f015ffa3
prasantsoy <94223490+prasantsoy@users.noreply.github.com>
2021-12-02 13:24:32 +0530

    Add files via upload

---
+++ 3_Implimentation/inc/code.h
@@ -0,0 +1,21 @@
+/**
+ * @file code.h
+ * @author Prasant Soy (prasantyos@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-02
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <avr/io.h>
+#define F_CPU 1000000
+#include <util/delay.h>
+#include <stdlib.h>
+
+#define enable            5
+#define registerselection 6
+
+void send_a_command(unsigned char command);
+void send_a_character(unsigned char character);
+void send_a_string(char *string_of_characters);
